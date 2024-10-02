7e236a8fabdf8ab52a337cc3b678046bdf238033
Sam Hastings <samhastingsis@gmail.com>
2024-10-02 11:54:39 -0400

    Create the FeedItem model.

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by sam hastings on 02/10/2024.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let image: URL
+}
+
