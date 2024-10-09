f1384d0bf9ed2ebc50eb873fe23254c01c6b9b08
Sam Hastings <samhastingsis@gmail.com>
2024-10-09 12:07:30 -0400

    Resume data task on creation.

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -7,28 +7,76 @@
 
 import XCTest
 
+class URLSessionHTTPClient {
+    private let session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    
+    func get(from url: URL) {
+        session.dataTask(with: url) { _, _, _ in  }.resume()
+    }
+}
+
 final class URLSessionHTTPClientTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_getFromURL_createsDataTaskWithURL() {
+        // Arrange
+        let url = URL(string: "https://any-url.com")!
+        let session = URLSessionSpy()
+        let sut = URLSessionHTTPClient(session: session)
+        
+        // Action
+        sut.get(from: url)
+        
+        // Assert
+        XCTAssertEqual(session.receivedURLs, [url])
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func test_getFromURL_resumesDataTaskWithURL() {
+        // Arrange
+        let url = URL(string: "https://any-url.com")!
+        let session = URLSessionSpy()
+        let task = URLSessionDataTaskSpy()
+        session.stub(url: url, task: task)
+        let sut = URLSessionHTTPClient(session: session)
+        
+        // Action
+        sut.get(from: url)
+        
+        // Assert
+        XCTAssertEqual(task.resumeCallCount, 1)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    // MARK: - Helpers
+    
+    private class URLSessionSpy: URLSession, @unchecked Sendable {
+        
+        var receivedURLs: [URL] = []
+        
+        private var stubs = [URL: URLSessionDataTaskSpy]()
+        
+        func stub(url: URL, task: URLSessionDataTaskSpy) {
+            stubs[url] = task
+        }
+        
+        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, (any Error)?) -> Void) -> URLSessionDataTask {
+            receivedURLs.append(url)
+            return stubs[url] ?? FakeURLSessionDataTask()
+        }
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    
+    private class FakeURLSessionDataTask: URLSessionDataTask, @unchecked Sendable {
+        override func resume() {}
+    }
+    
+    private class URLSessionDataTaskSpy: URLSessionDataTask, @unchecked Sendable {
+        var resumeCallCount = 0
+        
+        override func resume() {
+            resumeCallCount += 1
         }
     }
 
