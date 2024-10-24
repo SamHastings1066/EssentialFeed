8cfb46a2482e1cf3d2114413d716ec5435490c29
Sam Hastings <samhastingsis@gmail.com>
2024-10-24 09:09:18 -0400

    Move 'FeedCachePolicy' to its own file.

---
+++ EssentialFeed/Feed Cache/FeedCachePolicy.swift
@@ -0,0 +1,20 @@
+//
+//  FeedCachePolicy.swift
+//  EssentialFeed
+//
+//  Created by sam hastings on 24/10/2024.
+//
+
+internal final class FeedCachePolicy {
+    private init() {}
+    private static let calendar = Calendar(identifier: .gregorian)
+    
+    private static var maxCacheAgeInDays: Int {
+        return 7
+    }
+    
+    internal static func validate(_ timestamp: Date, against date: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else { return false }
+        return date < maxCacheAge
+    }
+}
