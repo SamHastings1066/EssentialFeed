f6ca6253a4b906f26b52e9394bcf312bcd7361a2
Sam Hastings <samhastingsis@gmail.com>
2024-10-25 10:30:19 -0400

    Retrieving after inserting into empty cache delivers inserted values.

--- EssentialFeed/Feed Cache/LocalFeedImage.swift
+++ EssentialFeed/Feed Cache/LocalFeedImage.swift
@@ -5,7 +5,7 @@
 //  Created by sam hastings on 21/10/2024.
 //
 
-public struct LocalFeedImage: Equatable {
+public struct LocalFeedImage: Equatable, Codable {
     public let id: UUID
     public let description: String?
     public let location: String?
