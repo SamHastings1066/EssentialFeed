e9a33c0ae61af6fe64d95361fc19e5bbda3b4be1
Sam Hastings <samhastingsis@gmail.com>
2024-11-01 09:38:28 -0400

    Fix typo.

--- EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -5,7 +5,7 @@
 //  Created by sam hastings on 21/10/2024.
 //
 
-public enum RetievedCacheFeedResult {
+public enum RetrievedCacheFeedResult {
     case empty
     case found(feed: [LocalFeedImage], timestamp: Date)
     case failure(Error)
@@ -14,7 +14,7 @@ public enum RetievedCacheFeedResult {
 public protocol FeedStore {
     typealias DeletionCompletion = (Error?) -> Void
     typealias InsertionCompletion = (Error?) -> Void
-    typealias RetrievalCompletion = (RetievedCacheFeedResult) -> Void
+    typealias RetrievalCompletion = (RetrievedCacheFeedResult) -> Void
 
     func deleteCachedFeed(completion: @escaping DeletionCompletion)
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
