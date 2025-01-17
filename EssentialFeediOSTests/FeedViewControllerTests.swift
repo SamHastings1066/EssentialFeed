775b44ab69b01adf1e85444f52080814c46cf0be
Sam Hastings <samhastingsis@gmail.com>
2025-01-17 10:15:34 -0500

    Hide loading indicator on loader completion.

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -19,6 +19,7 @@ final class FeedViewController: UITableViewController {
     }
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         refreshControl = UIRefreshControl()
         refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
         onViewIsAppearing = { vc in
@@ -36,7 +37,9 @@ final class FeedViewController: UITableViewController {
     }
     
     @objc private func load() {
-        loader?.load { _ in }
+        loader?.load { [weak self] _ in
+            self?.refreshControl?.endRefreshing()
+        }
     }
     
     @objc private func refresh() {
@@ -96,6 +99,14 @@ final class FeedViewControllerTests: XCTestCase {
         
     }
     
+    func test_viewDidLoad_hidesLoiadingIndicatorOnLoadCompletion() {
+        let (sut, loader) = makeSUT()
+        
+        sut.loadViewIfNeeded()
+        loader.completeFeedLoading()
+        XCTAssertEqual(sut.refreshControl?.isRefreshing, false)
+    }
+    
     // MARK: - Helpers
     
     func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
@@ -107,11 +118,19 @@ final class FeedViewControllerTests: XCTestCase {
     }
     
     class LoaderSpy: FeedLoader {
-        func load(completion: @escaping (FeedLoader.Result) -> Void) {
-            loadCallCount += 1
+        private var completions = [(FeedLoader.Result) -> Void]()
+        var loadCallCount: Int {
+            return completions.count
         }
         
-        var loadCallCount = 0
+        func load(completion: @escaping (FeedLoader.Result) -> Void) {
+            completions.append(completion)
+        }
+        
+        func completeFeedLoading() {
+            completions[0](.success([]))
+        }
+        
     }
     
     
