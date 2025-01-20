25d536bad530ad2cce9604685a07aa45756e01eb
Sam Hastings <samhastingsis@gmail.com>
2025-01-20 18:02:20 -0500

    Render loaded feed on successful load completion.

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by sam hastings on 20/01/2025.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
