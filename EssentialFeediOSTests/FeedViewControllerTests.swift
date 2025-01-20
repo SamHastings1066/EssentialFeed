7552c833bd96c34ada3545c38f9fa27bb119f037
Sam Hastings <samhastingsis@gmail.com>
2025-01-20 18:31:22 -0500

    Does not alter current feed rendering state on load error.

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -79,7 +79,19 @@ final class FeedViewControllerTests: XCTestCase {
         sut.simulateUserIniatedFeedReload()
         loader.completeFeedLoading(with: [image0, image1, image2, image3], at: 1)
         assertThat(sut, isRendering: [image0, image1, image2, image3])
+    }
+    
+    func test_loadCompletion_doesNotAlterCurrentRenderingStateOnError() {
+        let image0 = makeImage(description: "a description", location: "a location")
+        let (sut, loader) = makeSUT()
         
+        sut.loadViewIfNeeded()
+        loader.completeFeedLoading(with: [image0], at: 0)
+        assertThat(sut, isRendering: [image0])
+        
+        sut.simulateUserIniatedFeedReload()
+        loader.completeFeedLoadingWithError(at: 1)
+        assertThat(sut, isRendering: [image0])
     }
     
     // MARK: - Helpers
@@ -133,6 +145,10 @@ final class FeedViewControllerTests: XCTestCase {
             completions[index](.success(feed))
         }
         
+        func completeFeedLoadingWithError(at index: Int) {
+            let error = NSError(domain: "an error", code: 0)
+            completions[index](.failure(error))
+        }
     }
 }
 
